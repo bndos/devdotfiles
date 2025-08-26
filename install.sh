312aa9658770b57e55e4313d0fdb3fc1e2d712aa
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2025-08-26 18:39:36 -0400

    feat: remove any prompt

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+brew tap d12frosted/emacs-plus
 brew install emacs-plus@31 ripgrep
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-~/.config/emacs/bin/doom install
+~/.config/emacs/bin/doom install -y
 
 rm -rf ~/.config/doom
 git clone https://github.com/bndos/.doom.d ~/.config/doom
